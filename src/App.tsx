b7275b2545f3ad5e596435fc0c41def3d7c198ea
Adam Brawner <abrawner@ars.com>
2026-01-30 10:10:42 -0600

    autoformat

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,7 @@ import CrudDashboard from "./CrudDashboard";
 //import Dashboard from "./Dashboard";
 //import { ProTipApp } from "./ProTip";
 
-
 export default function App() {
-  return (
-    <CrudDashboard />
-  );
+	return <CrudDashboard />;
 }
 // can switch to ProTipApp, Dashboard, CrudDashboard
--- src/components/DashboardSidebarHeaderItem.tsx
+++ src/components/DashboardSidebarHeaderItem.tsx
@@ -1,46 +1,46 @@
-import * as React from 'react';
-import ListSubheader from '@mui/material/ListSubheader';
-import type {} from '@mui/material/themeCssVarsAugmentation';
-import DashboardSidebarContext from '../context/DashboardSidebarContext';
-import { DRAWER_WIDTH } from '../constants';
-import { getDrawerSxTransitionMixin } from '../mixins';
+import ListSubheader from "@mui/material/ListSubheader";
+import type {} from "@mui/material/themeCssVarsAugmentation";
+import * as React from "react";
+import { DRAWER_WIDTH } from "../constants";
+import DashboardSidebarContext from "../context/DashboardSidebarContext";
+import { getDrawerSxTransitionMixin } from "../mixins";
 
 export interface DashboardSidebarHeaderItemProps {
-  children?: React.ReactNode;
+	children?: React.ReactNode;
 }
 
 export default function DashboardSidebarHeaderItem({
-  children,
+	children,
 }: DashboardSidebarHeaderItemProps) {
-  const sidebarContext = React.useContext(DashboardSidebarContext);
-  if (!sidebarContext) {
-    throw new Error('Sidebar context was used without a provider.');
-  }
-  const {
-    mini = false,
-    fullyExpanded = true,
-    hasDrawerTransitions,
-  } = sidebarContext;
+	const sidebarContext = React.useContext(DashboardSidebarContext);
+	if (!sidebarContext) {
+		throw new Error("Sidebar context was used without a provider.");
+	}
+	const {
+		mini = false,
+		fullyExpanded = true,
+		hasDrawerTransitions,
+	} = sidebarContext;
 
-  return (
-    <ListSubheader
-      sx={{
-        fontSize: 12,
-        fontWeight: '600',
-        height: mini ? 0 : 36,
-        ...(hasDrawerTransitions
-          ? getDrawerSxTransitionMixin(fullyExpanded, 'height')
-          : {}),
-        px: 1.5,
-        py: 0,
-        minWidth: DRAWER_WIDTH,
-        overflow: 'hidden',
-        textOverflow: 'ellipsis',
-        whiteSpace: 'nowrap',
-        zIndex: 2,
-      }}
-    >
-      {children}
-    </ListSubheader>
-  );
+	return (
+		<ListSubheader
+			sx={{
+				fontSize: 12,
+				fontWeight: "600",
+				height: mini ? 0 : 36,
+				...(hasDrawerTransitions
+					? getDrawerSxTransitionMixin(fullyExpanded, "height")
+					: {}),
+				px: 1.5,
+				py: 0,
+				minWidth: DRAWER_WIDTH,
+				overflow: "hidden",
+				textOverflow: "ellipsis",
+				whiteSpace: "nowrap",
+				zIndex: 2,
+			}}
+		>
+			{children}
+		</ListSubheader>
+	);
 }
